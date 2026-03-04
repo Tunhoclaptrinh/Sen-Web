b15f727a44453d6533f501b08e35bc54ef9ade8a
Tunhoclaptrinh <tuannguyentien16@gmail.com>
2026-03-04 07:57:34 +0700

    fix

--- src/utils/analytics.ts
+++ src/utils/analytics.ts
@@ -5,7 +5,7 @@ const GA_MEASUREMENT_ID = import.meta.env.VITE_GA_MEASUREMENT_ID;
 export const initGA = () => {
   if (GA_MEASUREMENT_ID) {
     ReactGA.initialize(GA_MEASUREMENT_ID);
-    console.log("GA initialized with ID:", GA_MEASUREMENT_ID);
+    // console.log("GA initialized with ID:", GA_MEASUREMENT_ID);
   } else {
     console.warn("GA Measurement ID not found in environment variables.");
   }
